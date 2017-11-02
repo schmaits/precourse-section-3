b8e26f81069d11b38935f5bcf551746c65b0c2e1
schmaits <stansfield.c.r@gmail.com>
2017-11-02 08:46:05 +0000

    Complete

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 
 
 document.getElementById('submitbutton').addEventListener('mouseover', function(event){
-    event.target.style.color = "purple"
+    event.target.style.color = "purple";
 });
 
 
 document.getElementById('submitbutton').addEventListener('mouseout', function(event){
     event.target.style.color = 'black';
 });
+
+
